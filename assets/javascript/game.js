ce533e10b26c710efd31b6c1e7082ffa38045852
gbmahili <georgesmahili@gmail.com>
2017-11-19 18:36:00 -0500

    updated logic to make sure the crystal values updated when the user wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,127 @@
 $(document).ready(function () {
+    generateRandomNumber();
+    generateCrystalValues();
+    setCrystalsValues();
 
-    //1. Generate a random number between 19 and 120 to start the game:
-    var randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-    //##Set that random number in the randomNumberField;
-    var randomNumberField = $("#randomNumberField");
-    randomNumberField.text(randomNumber);
+    function generateRandomNumber() {
+        //1. Generate a random number between 19 and 120 to start the game:
+        randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+        //##Set that random number in the randomNumberField;
+        randomNumberField = $("#randomNumberField");
+        randomNumberField.text(randomNumber);
+    }
 
     //2. Generate 4 unique random numbers between 1 and 12 for each crystal:
-    var crystalValuesArray = []
-    while (crystalValuesArray.length < 4) {
-        var crystalValueRandomNumber = Math.ceil(Math.random() * 12)
-        if (crystalValuesArray.indexOf(crystalValueRandomNumber) > -1) continue;
-        crystalValuesArray[crystalValuesArray.length] = crystalValueRandomNumber;
+    function generateCrystalValues() {        
+    
+        crystalValuesArray = []
+        while (crystalValuesArray.length < 4) {
+            crystalValueRandomNumber = Math.ceil(Math.random() * 12)
+            if (crystalValuesArray.indexOf(crystalValueRandomNumber) > -1) continue;
+            crystalValuesArray[crystalValuesArray.length] = crystalValueRandomNumber;
+        }
+        return crystalValuesArray;
+
     }
 
     //##Set each Crystal with its random number in this order: red, blue, yellow, green:
-    var crystalValues = {
-        red: crystalValuesArray[0],
-        blue: crystalValuesArray[1],
-        yellow: crystalValuesArray[2],
-        green: crystalValuesArray[3]
+    function setCrystalsValues() {        
+    
+        crystalValues = {
+            red: crystalValuesArray[0],
+            blue: crystalValuesArray[1],
+            yellow: crystalValuesArray[2],
+            green: crystalValuesArray[3]
+        }
     }
 
-    //Set the value of the totalScore:
+    //Get the value of the totalScore:
     var totalScore = parseInt($("#totalScore").text());
+    //Get the value of the wins
+    var wins = parseInt($("#wins").text());
+    //Get the value of the losses
+    var losses = parseInt($("#losses").text());
     
     //On Red Click:
     $(".red").click(function(){
         //Set the data value and get it, then store it in a variable using chained methods..change it to a number:
-        var red = parseInt(($(this).data("value", crystalValues.red)).data("value"));
+        redValue = parseInt(($(this).data("value", crystalValues.red)).data("value"));
         //Add the red value to the value of score:
-        totalScore += red;
+        totalScore += redValue;
+        //Display the new score:
+        $("#totalScore").text(totalScore);
+        updateWinsLosses();
+        
+    });
+
+    //On Blue Click:
+    $(".blue").click(function () {
+        //Set the data value and get it, then store it in a variable using chained methods..change it to a number:
+        blueValue = parseInt(($(this).data("value", crystalValues.blue)).data("value"));
+        //Add the blue value to the value of score:
+        totalScore += blueValue;
         //Display the new score:
         $("#totalScore").text(totalScore);
         
+        updateWinsLosses();
+
     });
+
+    //On Blue Click:
+    $(".yellow").click(function () {
+        //Set the data value and get it, then store it in a variable using chained methods..change it to a number:
+        yellowValue = parseInt(($(this).data("value", crystalValues.yellow)).data("value"));
+        //Add the yellow value to the value of score:
+        totalScore += yellowValue;
+        //Display the new score:
+        $("#totalScore").text(totalScore);
+        updateWinsLosses();
+
+    });
+
+    //On Blue Click:
+    $(".green").click(function () {
+        //Set the data value and get it, then store it in a variable using chained methods..change it to a number:
+        greenValue = parseInt(($(this).data("value", crystalValues.green)).data("value"));
+        //Add the green value to the value of score:
+        totalScore += greenValue;
+        //Display the new score:
+        $("#totalScore").text(totalScore);
+        updateWinsLosses();
+
+    });
+
+    //Create function to check if totalScore is equal or greater than the random number
+    function updateWinsLosses(){
+        //Update Wins
+        if (totalScore == randomNumber) {
+            //Increase wins count
+            wins++;
+            //Update wins value field
+            $("#wins").text(wins);
+            generateRandomNumber(); 
+            totalScore = 0;
+            $("#totalScore").text(totalScore);
+            generateCrystalValues();
+            setCrystalsValues();
+
+        }
+
+        //Update losses
+        if (totalScore > randomNumber) {
+            //Increment losses value
+            losses++;
+            //Update losses value field
+            $("#losses").text(losses);
+            generateRandomNumber();
+            //We then set the totalScore back to 0;
+            totalScore = 0;
+            $("#totalScore").text(totalScore);
+            generateCrystalValues();
+            setCrystalsValues();
+        }
+        
+    }
+
     
 });
